89b7db61886595ebd477ccf63445b905892abf82
Leighzer <bwc_nothgiel@live.com>
2020-07-22 01:58:22 -0400

    v0.1.0;

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # TippyTap
+A simple c# console game that supplies random words from a large [(~370K)](https://github.com/dwyl/english-words/blob/master/words_dictionary.json) list for the user to type.
+
+Once all words are typed, the user is presented with a few basic stats on their typing.
+
+# Usage
+./TippyTap numberOfWordsToType
+./TippyTap
+
+Launching TippyTap without arguments launches it in 'infinity' mode.
---
+++ TippyTap/Program.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace TippyTap
+{
+    public class Program
+    {
+        public static List<string> WordCache = new List<string>();
+        public static Random random = new Random();
+
+        public static void Main(string[] argsArr)
+        {
+            LoadWordCache();
+
+            List<string> args = new List<string>(argsArr);
+
+            string arg = args.Count >= 1 ? args[0] : "";
+
+            int numberOfWordsToType;
+            if(arg == "")
+            {
+                numberOfWordsToType = 0;
+            }
+            else if(!int.TryParse(arg, out numberOfWordsToType))
+            {
+                Console.WriteLine("Invalid argument supplied.");
+                return;
+            }
+
+            string wordsToTypeMessage = numberOfWordsToType == 0 ? "infinity" : numberOfWordsToType.ToString();
+            Console.WriteLine($"Welcome to TippyTap! You have {wordsToTypeMessage} word(s) left to type. GO!");
+
+            int attempts = 0;
+            int remainingWordsToType = numberOfWordsToType;
+            bool gameCompleted = false;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            while (!gameCompleted)
+            {
+                string randomWord = GetRandomWord();
+                bool wordCompleted = false;
+                while (!wordCompleted)
+                {
+                    Console.WriteLine(randomWord);
+                    string userInput = Console.ReadLine();
+                    wordCompleted = string.Equals(userInput, randomWord);
+                    attempts++;
+                }
+                gameCompleted = --remainingWordsToType == 0;
+            }
+            stopWatch.Stop();
+
+            TimeSpan timeElapsed = stopWatch.Elapsed;
+
+            Console.WriteLine($"You have completed all {numberOfWordsToType} word(s)!");
+            Console.WriteLine($"This took you {timeElapsed.TotalSeconds.ToString()} seconds!");
+            Console.WriteLine($"You correctly typed {numberOfWordsToType} word(s) with {attempts} attempt(s). That is {((double)numberOfWordsToType/attempts).ToString("p2")} accuracy!");
+            return;
+        }
+
+        private static string GetRandomWord()
+        {
+            int randomIndex = random.Next(WordCache.Count);
+            return WordCache[randomIndex];
+        }
+
+        private static void LoadWordCache()
+        {
+            //using hash set to avoid possible dupes
+            HashSet<string> wordHashSet = new HashSet<string>();
+            using (StreamReader r = new StreamReader("words.json"))
+            {
+                string wordsJson = r.ReadToEnd();
+
+                JObject job = JObject.Parse(wordsJson);
+
+                foreach(var prop in job.Properties())
+                {
+                    wordHashSet.Add(prop.Name);
+                }
+            }
+            WordCache = wordHashSet.ToList();
+        }
+    }
+}
